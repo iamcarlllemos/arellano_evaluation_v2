375027be30f233f36959b93cf32d291b23e765cc
Carl Llemos <iamcarlllemos@gmail.com>
2024-03-05 05:50:54 +0800

    latest commit

--- resources/views/livewire/questionnaire-item.blade.php
+++ resources/views/livewire/questionnaire-item.blade.php
@@ -1,8 +1,8 @@
 <div>
     <h1 class="text-3xl font-semibold">Questionnaire Items</h1>
     <p class="text-sm font-medium mt-1 text-slate-900">List of all items in questionnaire created.</p>
-    <div class="w-full flex justify-between items-center gap-2 mt-5">
-        <div>
+    <div class="w-full flex justify-between items-center gap-2">
+        <div class="mt-[29px]">
             <a wire:navigate href="{{route('programs.questionnaire')}}" class="bg-slate-900 py-2 px-6 text-white text-sm font-bold rounded-md">Go Back</a>
         </div>
     </div>
