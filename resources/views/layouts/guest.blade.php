ea1645ed61b2aa5d01ebf6337c151353b4ad4336
Carl Llemos <iamcarlllemos@gmail.com>
2024-03-15 21:37:56 +0800

    latest commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,9 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-        <link rel="stylesheet" href="{{asset('assets/app.css')}}">
-        <script src="{{asset('assets/app.js')}}"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
